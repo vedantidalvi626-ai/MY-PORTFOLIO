938517f8dbe5e416495f63911b4e8b7460fc8e5d
Vedanti dalvi <vedantidalvi626@gmail.com>
2026-04-08 11:39:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'veda', 
-  database: 'portfolio'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'veda',
+  database: process.env.DB_NAME || 'portfolio'
 });
 
 db.connect(err => {
